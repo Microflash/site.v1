409797e5e54c4c867409dfd89f26b41d7db31987
Naiyer Asif <naiyer.app@gmail.com>
2020-08-30 14:51:37 +0530

    chore: refactor head elements configuration

--- app.config.js
+++ app.config.js
@@ -22,45 +22,18 @@ module.exports = {
     maxTocDepth: 3,
     outdationPeriod: 365 // in days
   },
-  headConfig: {
+  head: {
     meta: [
-      {
-        name: 'author',
-        content: pkgAuthor
-      },
-      {
-        name: 'description',
-        content: pkgDescription
-      },
-      {
-        name: 'theme-color',
-        content: '#101015'
-      }
+      { name: 'author', content: pkgAuthor },
+      { name: 'description', content: pkgDescription },
+      { name: 'theme-color', content: '#101015' }
     ],
     link: [
-      {
-        rel: 'icon',
-        href: '/favicon.svg'
-      },
-      {
-        rel: 'mask-icon',
-        href: '/mask-icon.svg',
-        color: '#db0b2f'
-      },
-      {
-        rel: 'apple-touch-icon',
-        href: '/apple-touch-icon.png'
-      },
-      {
-        rel: 'manifest',
-        href: '/manifest.json'
-      },
-      {
-        rel: 'preload',
-        href: '/assets/images/icons.svg',
-        type: 'image/svg+xml',
-        as: 'image'
-      }
+      { rel: 'icon', href: '/favicon.svg' },
+      { rel: 'mask-icon', href: '/mask-icon.svg', color: '#db0b2f' },
+      { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+      { rel: 'manifest', href: '/manifest.json' },
+      { rel: 'preload', href: '/assets/images/icons.svg', type: 'image/svg+xml', as: 'image' }
     ]
   },
   searchConfig: {
